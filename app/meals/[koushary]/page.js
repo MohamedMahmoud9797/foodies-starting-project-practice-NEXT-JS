47855534b83bebee66b8dae4d9576553fcb46419
mohamed <mohamedmahmoudme97@gmail.com>
2023-12-24 15:53:59 +0200

    init proj

---
+++ app/meals/[koushary]/page.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function koushary() {
+  return (
+   <h1 style={{ color: 'white', textAlign: 'center' }}> koushary</h1>
+  )
+}
---
+++ app/meals/share/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <main>
+   <h1 style={{ color: 'white', textAlign: 'center' }}>share</h1>
+    </main>
+  )
+}
